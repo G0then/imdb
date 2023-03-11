e0a3eaaddf5a3e3f72749876c60f5e3374e797ab
G0then <nelsonsilva19999@gmail.com>
2023-03-11 15:38:15 +0000

    My first nextjs 13 project

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from './head'
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head/>
       <body>{children}</body>
     </html>
   )
